1963f68f674f0ca2728d89787dd2175659d55a5d
jose am talavera <115177988+joseamtalavera@users.noreply.github.com>
2024-03-02 20:20:45 +0200

    revome test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// Remove the outdated test
+// test('renders learn react link', () => { /* ... */ });
+
+// Optionally, add new tests for your updated app
+// ... (new tests specific to your current application)
