302903e5762809f9c16662e59257e33162c5d84c
jose am talavera <115177988+joseamtalavera@users.noreply.github.com>
2024-04-16 20:00:56 +0300

    menu

--- client/src/components/Dashboard/Admin/DashAdmin/AdminDashboard.js
+++ client/src/components/Dashboard/Admin/DashAdmin/AdminDashboard.js
@@ -1,36 +1,27 @@
+//AdminDashboard.js
+
 import React from 'react';
-import { Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
-import { Button } from '@mui/material';
+import { Typography, Box, Link } from '@mui/material';
 import ResponsiveDrawerAdmin from '../../../Menu/ResponsiveDrawerAdmin';
 
 function AdminDashboard() {
-    // return (
-    //     <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-    //         <Typography variant="h4">
-    //             Welcome to the Admin Dashboard
-    //         </Typography>
-    //         <img src="./DALLE.png" alt="Dashboard illustration" style={{ marginTop: '20px', maxWidth: '300px' }} />
-    //     </div>
-    // );
+    
     return (
         <div>
-            <ResponsiveDrawerAdmin />
-            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            
-                <Typography variant="h4">
-                    Welcome to the Admin Dashboard!
-                </Typography>
-                {/* <Button 
-                    variant="contained" 
-                    component={Link} 
-                    to="/login" 
-                    style={{ marginTop: '20px', width: '300px', backgroundColor: 'orange' }}
-                >
-                    Logout
-                </Button> */}
-            </div>
-        </div>
+            <Box display="flex" flexDirection="column">
+                <Box>
+                    <Link to="/">
+                        <img src="/logo.png" alt="Logo" style={{ maxWidth: '125px', maxHeight: '75px', marginLeft: '50px', marginTop: '0px'}} /> {/* Logo image */}
+                    </Link>
+                </Box>
+                <ResponsiveDrawerAdmin />
+                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>      
+                    <Typography variant="h4">
+                        Welcome to the Admin Dashboard!
+                    </Typography>
+                </div>
+            </Box>
+        </div> 
     );
     
 }
--- client/src/components/Menu/ResponsiveDrawerAdmin.js
+++ client/src/components/Menu/ResponsiveDrawerAdmin.js
@@ -1,50 +1,83 @@
+//ResponsiveDrawerAdmin.js
+
 import React, { useState } from 'react';
-import { AppBar, Toolbar, IconButton, Typography, Drawer, List, ListItem, ListItemText, useMediaQuery, useTheme, Box, Button, Menu, MenuItem} from '@mui/material';
+import { AppBar, Toolbar, IconButton, Typography, Drawer, List, ListItem, ListItemText, useMediaQuery, useTheme} from '@mui/material';
 import { Link } from 'react-router-dom';
 import MenuIcon from '@mui/icons-material/Menu';
 import Collapse from '@mui/material/Collapse';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import { ListItemIcon } from '@mui/material';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import UserIcon from '@mui/icons-material/Person';
+import CalendarIcon from '@mui/icons-material/CalendarToday';
+import BookingsIcon from '@mui/icons-material/Book';
+import InvoicingIcon from '@mui/icons-material/Receipt';
+
+
 
 function ResponsiveDrawerAdmin() {
     const [mobileOpen, setMobileOpen] = useState(false);
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down(900));
     const [open, setOpen] = useState(false);
-    const [anchorEl, setAnchorEl] = useState(null);
+    
 
     const handleDrawerToggle = () => {
         setMobileOpen(!mobileOpen);
+        setOpen(!open);
     };
     const handleClick = () => {
         setOpen(!open);
     };
 
+    const listItems = [
+        { text: 'Users', icon: <UserIcon /> },
+        { text: 'Calendar', icon: <CalendarIcon /> },
+        { text: 'Bookings', icon: <BookingsIcon /> },
+        { text: 'Invoicing', icon: <InvoicingIcon /> },
+        // Add more items as needed
+    ];
+
+
+
     const drawer = (
         <div> 
     
             <List>
-                {['Users', 'Calendar', 'Bookings', 'Invoicing'].map((text, index) => (
+                {/* {['Users', 'Calendar', 'Bookings', 'Invoicing'].map((text, index) => ( */}
+                {listItems.map(({text, icon }, index) => (
                     <ListItem button key={text}>
-                        <ListItemText primary={text} style={{color:'orange'}} />
+                        <ListItemIcon> 
+                                {React.cloneElement(icon, { style: { fontSize: '18px', color: 'black'} })}
+                        </ListItemIcon>
+                        <ListItemText primary={ 
+                            <Typography variant="body2" style={{color:'black', fontSize:'14px'}}>
+                                {text}
+                            </Typography>}/>
                     </ListItem>
                 ))}
                 <ListItem button onClick={handleClick}>
-                    <ListItemText primary="More +" style={{color:'orange'}} />
+                    <ListItemText primary="More +" style={{color:'black'}} />
                     {open ? <ExpandLess /> : <ExpandMore />}
                 </ListItem>
                 <Collapse in={open} timeout="auto" unmountOnExit>
                     <List component="div" disablePadding>
                         {['Mantenimiento', 'Reviews', 'Centres', 'Product','Settings', 'Help'].map((text, index) => (
                             <ListItem button key={text} style={{paddingLeft: '30px'}}>
-                                <ListItemText primary={text} style={{color:'orange'}} />
+                                <ListItemText primary={
+                                    <Typography variant="body2" style={{color:'grey', fontSize:'14px'}}>
+                                    {text}
+                                </Typography>}/>
                             </ListItem>
                         ))}
                     </List>
                 </Collapse>
                 <ListItem button key="Logout" component={Link} to="/login">
-                    <ListItemText primary="Logout" style={{color:'orange'}} />
+                    <ListItemText primary={
+                                    <Typography variant="body2" style={{color:'black', fontSize:'14px'}}>
+                                    Logout
+                                </Typography>}/>
                 </ListItem>
             </List>
         </div>
@@ -52,16 +85,18 @@ function ResponsiveDrawerAdmin() {
 
     return (
         <div>
-            <AppBar position="static" style={{ background: 'white' }} elevation={0}>
+            {/* Moved the AppBar outside the Drawer */}
+            <AppBar position="fixed" style={{zIndex: theme.zIndex.drawer +1, backgroundColor: 'white', color: 'black', boxShadow:'none'}}>
                 <Toolbar style={{ display: 'flex', justifyContent: 'space-between'}}>
-                <Box>
-                    <Link to="/">
+                <Link to="/">
                         <img src="/logo.png" alt="Logo" style={{ maxWidth: '125px', maxHeight: '75px', marginLeft: '50px', marginTop: '0px'}} /> {/* Logo image */}
                     </Link>
-                </Box>
+                    {/* <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Header Title
+                    </Typography> */}
                     {isMobile ? (
                         <IconButton
-                            color="inherit"
+                            //color="orange"
                             aria-label="open drawer"
                             edge="start"
                             onClick={handleDrawerToggle}
@@ -71,30 +106,33 @@ function ResponsiveDrawerAdmin() {
                         </IconButton>
                     ) : null}
                 </Toolbar>
-                
-                     
-                    <Drawer
-                        variant={isMobile ? "temporary" : "permanent"}
-                        anchor="left"
-                        open={!isMobile || mobileOpen}
-                        onClose={handleDrawerToggle}
-                        ModalProps={{
-                            keepMounted: true, // Better open performance on mobile.
-                        }}
-                        PaperProps={{
-                            style: {
-                                width: '30%',
-                                background: 'white',
-                                color: 'orange',
-                                border: '1px solid orange '
-                            }
-                        }}
-                    >
-                        {drawer}
-                    </Drawer>
             </AppBar>
+            {/* Drawer component */}
+            <Drawer
+                variant={isMobile ? "temporary" : "permanent"}
+                anchor="left"
+                open={!isMobile ? mobileOpen: open}
+                onClose={handleDrawerToggle}
+                ModalProps={{
+                    keepMounted: true, // Better open performance on mobile.
+                }}
+                PaperProps={{
+                    style: {
+                        width: '20%',
+                        background: 'white',
+                        color: 'black',
+                        boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)',
+                        marginTop: 64 // Adjust this value as needed to position the drawer below the header
+                    }
+                }}
+            >
+                {drawer}
+            </Drawer>
+            {/* Placeholder div to prevent content from being overlapped by the fixed AppBar */}
+            {!isMobile && <div style={{ height: 64 }} />} {/* Placeholder div */}
         </div>
     );
 }
 
 export default ResponsiveDrawerAdmin;
+ 
