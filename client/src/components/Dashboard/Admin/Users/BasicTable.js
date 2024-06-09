7d25bde394392d4205f255da17bb0ec57813638e
jose am talavera <115177988+joseamtalavera@users.noreply.github.com>
2024-06-09 14:45:15 +0300

    delete button

--- client/src/components/Dashboard/Admin/Users/BasicTable.js
+++ client/src/components/Dashboard/Admin/Users/BasicTable.js
@@ -2,29 +2,85 @@
 // BasicTable.js
 import React, { useEffect, useState } from 'react';
 import { DataGrid, GridColDef, GridToolbar } from '@mui/x-data-grid';
-import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import { useNavigate} from 'react-router-dom';
 import CircularProgress from '@mui/material/CircularProgress';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import IconButton from '@mui/material/IconButton';
 
-
-const columns = [
-    /* { field: 'id', headerName: 'ID', width: 100, hide: true }, */
-    { field: 'name', headerName: 'Name', width: 250, filterable: true },
-    { field: 'email', headerName: 'Email', width: 250, filterable: true },
-    { field: 'category', headerName: 'Category', width: 250, filterable: true },
-    { field: 'status', headerName: 'Status', width: 175, filterable: true },
-    /* { field: 'created', headerName: 'Created', width: 175, filterable: true, type: 'date' }, */
-  
-];
 
 export default function DataTable() {
 
     const navigate = useNavigate();
     const [users, setUsers] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    
 
+    const columns = [
+        //{ field: 'id', headerName: 'ID', width: 100, hide: true }, 
+        { field: 'name', headerName: 'Name', width: 300, filterable: true },
+        { field: 'email', headerName: 'Email', width: 200, filterable: true },
+        /* {
+            field: 'nameAndEmail',
+            headerName: 'Name',
+            width: 250,
+            filterable: true,
+            renderCell: (params) => {
+                const user = params.row;
+                if(!user) {
+                    console.log('User not found');
+                    return null;
+                }
+                if(!user.email) {
+                    console.log('Email not found');
+                    return null;
+                }
+
+                console.log(user.name, user.email);
+
+                return (
+                    
+                    <div>
+                    {user.name ? user.name : ''}<br />{user.email}
+                </div>
+                )
+            }
+        }, */
+        { field: 'phone', headerName: 'Phone', width: 200, filterable: true},
+        { field: 'category', headerName: 'Category', width: 175, filterable: true },
+        { field: 'status', headerName: 'Status', width: 175, filterable: true },
+        //{ field: 'created', headerName: 'Created', width: 200, filterable: true, type: 'date' },
+        { 
+            field: 'delete', 
+            headerName: ' ', 
+            sortable: false,
+            width: 100, 
+            disableClickEventBubbling: true,
+            renderCell: (params) => {
+                const onClick = async () => {
+                    const id = params.row.id;
+                    const response = await fetch(`${process.env.REACT_APP_API_URL}/api/users/${id}`, {
+                        method: 'DELETE',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                    });
+                    if (!response.ok) {
+                        throw new Error('Failed to delete user');
+                    }
+                    setUsers(users.filter(user => user.id !== id));
+                };
+                return (
+                    <IconButton style={{color: 'orange'}} onClick={onClick}>
+                        <DeleteOutlineIcon />
+                    </IconButton>
+                );
+                
+            }
+        },
+    ];
+ 
     useEffect(() => {
         const fetchUsers = async () => { 
             setIsLoading(true);
@@ -86,16 +142,17 @@ export default function DataTable() {
     
     return (
        /*  <Container maxWidth="lg"> */
-            <Box sx={{ maxWidth: 'lg', margin: 'auto', boxShadow: 3, height: 500, width: '90%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <Box sx={{ maxWidth: 'lg', margin: 'auto', boxShadow: 3, height: 600, width: '90%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                 {isLoading ? (
                     <CircularProgress />
                 ) : (
            
             <DataGrid
-                style={{ height: 500, width: '100%', cursor: 'pointer'}}
+                style={{ height: 600, width: '100%', cursor: 'pointer'}}
                 rows={users}
                 columns={columns}
                 pageSize={25}
+                rowHeight={75}
                 rowsPerPageOptions={[10, 25, 50, 100]}
                 checkboxSelection
                 components={{
