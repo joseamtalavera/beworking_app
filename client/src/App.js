7b3dc13d0abfe28286663f51bf963db22c32b10a
jose am talavera <115177988+joseamtalavera@users.noreply.github.com>
2024-08-11 13:16:54 +0300

    remove drawer

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Typography} from '@mui/material';
-import ResponsiveDrawer from './components/Menu/ResponsiveDrawer';
+//import ResponsiveDrawer from './components/Menu/ResponsiveDrawer';
 
 // Main component
 function App() {
   return (
     <div>
-      <ResponsiveDrawer />
+      {/* <ResponsiveDrawer /> */}
       <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
         <Typography variant="h4">
           Welcome to the BeWorking new Website!
